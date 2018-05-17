b309062e5f70086134964b3111e195ee5b9ab37f
kasper@ottzen.com <kasper@ottzen.com>
2018-05-17 11:02:17 +0200

    source files

---
+++ src/app.js
@@ -0,0 +1,12 @@
+// Styles
+const mainStyle = require('./scss/main.scss');
+const headerStyle = require('./scss/header/header.scss');
+const pagebannerStyle = require('./scss/page-banner/page-banner.scss');
+const spotboxStyle = require('./scss/spot-box/spot-box.scss');
+const listStyle = require('./scss/list/list.scss');
+const btn = require('./scss/base/button.scss');
+
+// Javasript
+import { person, sayHello } from './js/lib';
+import * as getposts from './js/getposts';
+
---
+++ src/js/lib.js
@@ -0,0 +1,26 @@
+export const person = [
+    {
+        name: 'Kasper Ottzen',
+        location: 'Aarhus',
+        age: '34'
+    },
+    {
+        name: 'Emilie Eibner',
+        location: 'Viby J',
+        age: '35'
+    },
+    {
+        name: 'Patricia Ottzen Eibner',
+        location: 'Viby J',
+        age: '7'
+    },
+    {
+        name: 'Isabella Ottzen Eibner',
+        location: 'Viby J',
+        age: '6'
+    }
+]
+
+export function sayHello(name) {
+    return `Hello ${name}`;
+}
